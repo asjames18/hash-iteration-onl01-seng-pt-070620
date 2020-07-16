1259617c40d5d3ce7d44380e8b6a62e3016fe060
Antonio James <asjames18@gmail.com>
2020-07-16 00:58:39 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,7 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
-    if age > 12 
+    if age > 12
     Puts "You are too old for this."
   else
     birthday_kids
