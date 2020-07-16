1d5b4b7ce550c389cffbb8b291868ef397c8dd22
Antonio James <asjames18@gmail.com>
2020-07-16 01:02:09 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -16,7 +16,7 @@ def age_appropriate_birthday(birthday_kids)
     if "#{age}" > 12
     puts "You are too old for this."
   else
-    "Happy Birthday #{name}! You are now #{age} years old!"
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
 
 end
