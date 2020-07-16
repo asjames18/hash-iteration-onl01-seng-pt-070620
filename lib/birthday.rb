d23b774734d3accb90cffa217e3f184bf277a4ae
Antonio James <asjames18@gmail.com>
2020-07-16 00:46:15 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,10 +9,8 @@ def happy_birthday(birthday_kids)
 birthday_kids.each do |name, age|
     puts "Happy Birthday #{name}! You are now #{age} years old!"
   end
+end
+
 def age_appropriate_birthday(array)
-
-  
-    end
-
 
 end
